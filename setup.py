ae51f4491407b80acfddaa8a7abd17a03f19819a
Stefan Andres <s.andres@syseleven.de>
2015-03-26 09:51:30 +0100

    use PBR

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
-from setuptools import setup
+#!/usr/bin/env python
+# Copyright (c) 2014 Syseleven GmbH
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-setup(name='cloudutils-libs',
-      description='Useful libs for interacting with OpenStack',
-      author='Stefan Andres',
-      author_email='s.andres@syseleven.de',
-      license='Apache',
-      packages=['syseleven.cloudutilslibs'],
-      zip_safe=False)
+import setuptools
+
+setuptools.setup(
+    setup_requires = ['pbr'],
+    pbr=True
+)
